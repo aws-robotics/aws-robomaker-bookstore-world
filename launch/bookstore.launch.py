5f2a88a4eb2d61668930ba7b660e8dca7b2d9e23
Tim Robinson <tim@amazon.com>
2019-09-10 14:49:03 -0700

    HACK: Make bookstore-world work with cloudwatch

--- launch/bookstore.launch.py
+++ launch/bookstore.launch.py
@@ -8,9 +8,17 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     world_file_name = "bookstore.world"
-    world = os.path.join('worlds', world_file_name)
+    world = os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'worlds', world_file_name)
     env = {
-        'GAZEBO_MODEL_PATH': os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'models')
+        'GAZEBO_MODEL_PATH': ":".join([
+            '/usr/share/gazebo-9/models', 
+            os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'models'),
+            os.path.split(get_package_share_directory('turtlebot3_description_reduced_mesh'))[0],
+        ]),
+        'GAZEBO_RESOURCE_PATH': ":".join([
+            '/usr/share/gazebo-9', 
+            os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'))
+        ])
     }
 
     ld = launch.LaunchDescription([
@@ -19,7 +27,7 @@ def generate_launch_description():
             default_value='false'
         ),
         launch.actions.ExecuteProcess(
-            cmd=['gzserver', '--verbose', world],
+            cmd=['gzserver', '--verbose', world, '-s', 'libgazebo_ros_factory.so'],
             cwd=get_package_share_directory('aws_robomaker_bookstore_world'),
             additional_env=env,
             output='screen'
