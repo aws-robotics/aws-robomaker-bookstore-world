adbb2bf2489a4e0092b25a5bdcf181e0d3cf1824
Tim Robinson <tim@amazon.com>
2019-09-10 18:48:49 -0700

    Add ability to specify additional gazebo_model_paths
    - When launching gzserver add ability to add additional gazebo model
    paths so we can add it for the turtlebot etc without hard coding it into
    the bookstore world.

--- launch/bookstore.launch.py
+++ launch/bookstore.launch.py
@@ -3,6 +3,7 @@ import sys
 
 import launch
 from launch.conditions import IfCondition
+from launch.substitutions import PythonExpression
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -10,11 +11,11 @@ def generate_launch_description():
     world_file_name = "bookstore.world"
     world = os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'worlds', world_file_name)
     env = {
-        'GAZEBO_MODEL_PATH': ":".join([
-            '/usr/share/gazebo-9/models', 
-            os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'models'),
-            os.path.split(get_package_share_directory('turtlebot3_description_reduced_mesh'))[0],
-        ]),
+        'GAZEBO_MODEL_PATH': PythonExpression(['":".join([',
+            "'/usr/share/gazebo-9/models',", 
+            "'", os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'models'), "',",
+            "'", launch.substitutions.LaunchConfiguration('gazebo_model_path'), "',"
+        '])']),
         'GAZEBO_RESOURCE_PATH': ":".join([
             '/usr/share/gazebo-9', 
             os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'))
@@ -25,6 +26,10 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(
             name='gui',
             default_value='false'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='gazebo_model_path',
+            default_value=''
         ),
         launch.actions.ExecuteProcess(
             cmd=['gzserver', '--verbose', world, '-s', 'libgazebo_ros_factory.so'],
