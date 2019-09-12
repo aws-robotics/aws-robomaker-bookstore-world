a6c81cee7b440603a5b9dafc47627fe1cf5c355d
Avishay Alon <avishaya@amazon.com>
2019-09-12 01:58:49 +0000

    Use gazebo_ros' launch scripts

--- launch/bookstore.launch.py
+++ launch/bookstore.launch.py
@@ -10,40 +10,30 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     world_file_name = "bookstore.world"
     world = os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'worlds', world_file_name)
-    env = {
-        'GAZEBO_MODEL_PATH': PythonExpression(['":".join([',
-            "'/usr/share/gazebo-9/models',", 
-            "'", os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'), 'models'), "',",
-            "'", launch.substitutions.LaunchConfiguration('gazebo_model_path'), "',"
-        '])']),
-        'GAZEBO_RESOURCE_PATH': ":".join([
-            '/usr/share/gazebo-9', 
-            os.path.join(get_package_share_directory('aws_robomaker_bookstore_world'))
-        ])
-    }
+
+    gazebo_ros = get_package_share_directory('gazebo_ros')
+    gazebo_client = launch.actions.IncludeLaunchDescription(
+	launch.launch_description_sources.PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros, 'launch', 'gzclient.launch.py')),
+        condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration('gui'))
+     )
+    gazebo_server = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros, 'launch', 'gzserver.launch.py'))
+    )
+
 
     ld = launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+          'world',
+          default_value=[world, ''],
+          description='SDF world file'),
         launch.actions.DeclareLaunchArgument(
             name='gui',
             default_value='false'
         ),
-        launch.actions.DeclareLaunchArgument(
-            name='gazebo_model_path',
-            default_value=''
-        ),
-        launch.actions.ExecuteProcess(
-            cmd=['gzserver', '--verbose', world, '-s', 'libgazebo_ros_factory.so'],
-            cwd=get_package_share_directory('aws_robomaker_bookstore_world'),
-            additional_env=env,
-            output='screen'
-        ),
-        launch.actions.ExecuteProcess(
-            cmd=['gzclient'],
-            additional_env=env,
-            output='screen',
-            condition=IfCondition(
-                launch.substitutions.LaunchConfiguration('gui'))
-        ),
+        gazebo_server,
+        gazebo_client
     ])
     return ld
 
